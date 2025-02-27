4c6648a4242ebc144cb00cbe56d800d80440e177
unknown <sokda106@gmail.com>
2025-02-27 13:12:26 +0700

    folder changed

---
+++ lib/w7-blablacar/service/locations_service.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_y3_t2/w7-blablacar/model/ride/locations.dart';
+
+import '../dummy_data/dummy_data.dart';
+
+////
+///   This service handles:
+///   - The list of available rides
+///
+class LocationsService {
+  static const List<Location> availableLocations =
+      fakeLocations; // TODO for now fake data
+}
